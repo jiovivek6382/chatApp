c721c6372c5ac33d42d089e3acfa635c7dfab39e
jiovivek6382 <62273005+jiovivek6382@users.noreply.github.com>
2020-03-17 08:47:14 +0530

    Upload All files

---
+++ scripts/firebase.init.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA-N7bwMhJRC1hqDwRXAZtW2FJu54uh3sc",
+    authDomain: "chat-app-9af3e.firebaseapp.com",
+    databaseURL: "https://chat-app-9af3e.firebaseio.com",
+    projectId: "chat-app-9af3e",
+    storageBucket: "chat-app-9af3e.appspot.com",
+    messagingSenderId: "39364130335",
+    appId: "1:39364130335:web:a57c7a7065568e7407872a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
