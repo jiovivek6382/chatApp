fcf17f57495bfd2cd0f26e0e919c6e723fe7699f
jiovivek6382 <62273005+jiovivek6382@users.noreply.github.com>
2020-03-18 07:55:32 +0530

    Update firebase.init.js

--- scripts/firebase.init.js
+++ scripts/firebase.init.js
@@ -1,12 +1,12 @@
 ﻿// Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBxhkOKc_tWaeeUSePIwitc2Tm-aqrLTww",
-    authDomain: "chat-app-7da13.firebaseapp.com",
-    databaseURL: "https://chat-app-7da13.firebaseio.com",
-    projectId: "chat-app-7da13",
-    storageBucket: "",
-    messagingSenderId: "454878238535",
-    appId: "1:454878238535:web:6fb06475d86f38e4"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+    apiKey: "AIzaSyA-N7bwMhJRC1hqDwRXAZtW2FJu54uh3sc",
+    authDomain: "chat-app-9af3e.firebaseapp.com",
+    databaseURL: "https://chat-app-9af3e.firebaseio.com",
+    projectId: "chat-app-9af3e",
+    storageBucket: "chat-app-9af3e.appspot.com",
+    messagingSenderId: "39364130335",
+    appId: "1:39364130335:web:a57c7a7065568e7407872a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
