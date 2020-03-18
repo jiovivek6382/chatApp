eb12c7eb3a57e7679c3298799d18e5d8269a1cb7
jiovivek6382 <62273005+jiovivek6382@users.noreply.github.com>
2020-03-18 08:51:00 +0530

    firebase messaging

--- messaging/firebase-messaging-sw.js
+++ messaging/firebase-messaging-sw.js
@@ -1,20 +1,20 @@
 ﻿// Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/7.11.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.11.0/firebase-messaging.js');
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBxhkOKc_tWaeeUSePIwitc2Tm-aqrLTww",
-    authDomain: "chat-app-7da13.firebaseapp.com",
-    databaseURL: "https://chat-app-7da13.firebaseio.com",
-    projectId: "chat-app-7da13",
-    messagingSenderId: "454878238535",
-    appId: "1:454878238535:web:6fb06475d86f38e4"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+    apiKey: "AIzaSyA-N7bwMhJRC1hqDwRXAZtW2FJu54uh3sc",
+    authDomain: "chat-app-9af3e.firebaseapp.com",
+    databaseURL: "https://chat-app-9af3e.firebaseio.com",
+    projectId: "chat-app-9af3e",
+    messagingSenderId: "39364130335",
+    appId: "1:39364130335:web:a57c7a7065568e7407872a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
